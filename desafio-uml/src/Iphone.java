5f1a49bfea3c4e6381d33f5f09f0f411be1341c4
leandro <leandronatalio.dev@gmail.com>
2024-05-14 16:12:34 -0300

    Desafio: POO - Iphone

---
+++ desafio-uml/src/Iphone.java
@@ -0,0 +1,60 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, Navegador {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música");
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando telefone");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+}
